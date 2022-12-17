a3db90236a8e6f7998befd1e6713891110605a73
netshade <adriano.condro@gmail.com>
2022-12-17 14:05:08 +0100

    os_detection

--- Openvas_engine.py
+++ Openvas_engine.py
@@ -136,7 +136,45 @@ def update_statistic_vul(id_j):
     stat_host.execute(sql_update_query, update_data)
     conn.commit()
 
+def test():
 
+    global token_ver
+    while True:
+        conn_check = DB_connect.database_connect()
+        conn = conn_check.database_connection()
+        cur = conn.cursor()
+
+        id_ass = "144"
+        id_asset = list()
+        id_asset.append(id_ass)
+
+        try:
+            sql_query_token = """SELECT token FROM engines WHERE engines.codeword = %s; """
+            input_data = id_asset
+            cur.execute(sql_query_token, input_data)
+
+            token = cur.fetchone()
+            token = token[0]
+
+        except:
+            print("nessun token rilevato")
+
+        if token_ver != token:
+            try:
+                sql_update_query = """UPDATE engines SET active_openvas = %s WHERE engines.codeword = %s; """
+                input_data = (token, id_ass)
+                cur.execute(sql_update_query, input_data)
+                conn.commit()
+                token_ver = token
+            except:
+                print("verifica token non effettuata")
+
+        print(datetime.now())
+        print("\ntoken attuale verificato: "+token)
+        time.sleep(10)
+
+t = Thread(target=test)
+t.start()
 
 
 while True:
@@ -144,7 +182,7 @@ while True:
     try:
         data = json.load(open("eng_conf.json"))
     except:
-        print("Engine non inizializzato! eseguire: ./inizializzazione_engine.py ")
+        print("Engine vulnscan non inizializzato! eseguire: ./inizializzazione_engine.py ")
         sys.exit(1)
 
 
