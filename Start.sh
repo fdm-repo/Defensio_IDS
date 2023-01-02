43cfd782a988f819a8c8c57b97eb0a3be77127cc
netshade <adriano.condro@gmail.com>
2023-01-02 23:47:31 +0100

    avvio

--- Start.sh
+++ Start.sh
@@ -2,11 +2,11 @@
 ./inizializzazione_engine.py
 
 gnome-terminal -- ./crea_token.py
-
-gnome-terminal -- ./Defensio_engine.py
-
-gnome-terminal -- ./Openvas_engine.py
-
-gnome-terminal -- ./ShareScanner_engine.py
-
 gnome-terminal -- ./WebScanner_engine.py
+
+gnome-terminal -- sudo ./Defensio_engine.py
+
+gnome-terminal -- sudo ./Openvas_engine.py
+
+gnome-terminal -- sudo ./ShareScanner_engine.py
+
