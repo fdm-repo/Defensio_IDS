7383382f1348872bba96a467a242c04a8cec80fa
netshade <adriano.condro@gmail.com>
2023-01-04 17:33:45 +0100

    installazione e configurazione

--- Installazione_PHPmyAdmin.sh
+++ Installazione_PHPmyAdmin.sh
@@ -1,5 +1,3 @@
-sudo apt install -y apache2 apache2-utils;
-sudo apt install -y mariadb-server mariadb-client;
-sudo apt install -y php php-mysql libapache2-mod-php;
+sudo apt install -y phpmyadmin;
 
 
