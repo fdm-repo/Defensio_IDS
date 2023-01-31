0fe0a4e8cc5342371f09479926d4dc739aefbddc
netshade <adriano.condro@gmail.com>
2023-01-31 21:00:04 +0100

    UpgradeDB

--- Start.sh
+++ Start.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/bash
-./inizializzazione_engine.py
+echo "
+ ______  _______ _______ _______ __   _ _______ _____  _____       _______ __   _  ______ _____ __   _ _______ _______
+ |     \ |______ |______ |______ | \  | |______   |   |     |      |______ | \  | |  ____   |   | \  | |______ |______
+ |_____/ |______ |       |______ |  \_| ______| __|__ |_____|      |______ |  \_| |_____| __|__ |  \_| |______ ______|
 
-gnome-terminal \
---tab --title="GenerazioneToken" -e "./crea_token.py" \
---tab --title="WebScanner" -e "./WebScanner_engine.py" \
---tab --title="Scheduling" -e "./Scheduling.py" \
---tab --title="DPI" -e "./Email_Leaks.py" \
---tab --title="NetScanner" -e "sudo ./Defensio_engine.py" \
---tab --title="VulnScan" -e "sudo ./Openvas_engine.py" \
---tab --title="ShareScan" -e "sudo ./ShareScanner_engine.py --tab"
+By FIDEM.srl
+"
+echo "Si deve inizializzare l'engine?"
+read -p 'Y/N: ' input
+
+if [[ $input == "Y" || $input == "y" ]]; then
+    ./inizializzazione_engine.py
+fi
+
+gnome-terminal --tab --title="GenerazioneToken" -- ./crea_token.py
+gnome-terminal --tab --title="WebScanner" -- ./WebScanner_engine.py
+gnome-terminal --tab --title="Scheduling" -- ./Scheduling.py
+gnome-terminal --tab --title="DPI" -- ./Email_Leaks.py
+gnome-terminal --tab --title="NetScanner" -- sudo ./Defensio_engine.py
+gnome-terminal --tab --title="VulnScan" -- sudo ./Openvas_engine.py
+gnome-terminal --tab --title="ShareScan" -- sudo ./ShareScanner_engine.py
 
