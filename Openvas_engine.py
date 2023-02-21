4aa77e2faaa40305cfd1c29485a5db899cba15b3
n3tSh4d3 <adriano.condro@gmail.com>
2023-02-21 15:09:23 +0100

    Update Openvas_engine.py

--- Openvas_engine.py
+++ Openvas_engine.py
@@ -15,7 +15,7 @@ import parsing_xml_report_NetScanner
 # username e password per il docker GVM
 
 username = 'admin'
-password = 'porcodio'
+password = '!d3f3n510!'
 
 idprocess = "Openvas_engine"
 
